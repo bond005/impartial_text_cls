483f794027887de6be54b144c8dc167f7b3d6ea4
Ivan Bondarenko <bond005@yandex.ru>
2019-10-01 18:17:37 +0700

    Initializing of neurons has been changed.

--- impatial_text_cls/impatial_text_cls.py
+++ impatial_text_cls/impatial_text_cls.py
@@ -814,35 +814,35 @@ class ImpatialTextClassifier(BaseEstimator, ClassifierMixin):
             if self.filters_for_conv1 > 0:
                 conv_layer_1 = tfp.layers.Convolution1DFlipout(
                     filters=self.filters_for_conv1, kernel_size=1, name='Conv1', padding='valid',
-                    activation=tf.nn.relu, seed=self.random_seed
+                    activation=tf.nn.tanh, seed=self.random_seed
                 )(input_sequence_layer)
                 conv_layer_1 = tf.keras.layers.GlobalMaxPooling1D(name='MaxPooling1')(conv_layer_1)
                 conv_layers.append(conv_layer_1)
             if self.filters_for_conv2 > 0:
                 conv_layer_2 = tfp.layers.Convolution1DFlipout(
                     filters=self.filters_for_conv2, kernel_size=2, name='Conv2', padding='valid',
-                    activation=tf.nn.relu, seed=self.random_seed
+                    activation=tf.nn.tanh, seed=self.random_seed
                 )(input_sequence_layer)
                 conv_layer_2 = tf.keras.layers.GlobalMaxPooling1D(name='MaxPooling2')(conv_layer_2)
                 conv_layers.append(conv_layer_2)
             if self.filters_for_conv3 > 0:
                 conv_layer_3 = tfp.layers.Convolution1DFlipout(
                     filters=self.filters_for_conv3, kernel_size=3, name='Conv3', padding='valid',
-                    activation=tf.nn.relu, seed=self.random_seed
+                    activation=tf.nn.tanh, seed=self.random_seed
                 )(input_sequence_layer)
                 conv_layer_3 = tf.keras.layers.GlobalMaxPooling1D(name='MaxPooling3')(conv_layer_3)
                 conv_layers.append(conv_layer_3)
             if self.filters_for_conv4 > 0:
                 conv_layer_4 = tfp.layers.Convolution1DFlipout(
                     filters=self.filters_for_conv4, kernel_size=4, name='Conv4', padding='valid',
-                    activation=tf.nn.relu, seed=self.random_seed
+                    activation=tf.nn.tanh, seed=self.random_seed
                 )(input_sequence_layer)
                 conv_layer_4 = tf.keras.layers.GlobalMaxPooling1D(name='MaxPooling4')(conv_layer_4)
                 conv_layers.append(conv_layer_4)
             if self.filters_for_conv5 > 0:
                 conv_layer_5 = tfp.layers.Convolution1DFlipout(
                     filters=self.filters_for_conv5, kernel_size=5, name='Conv5', padding='valid',
-                    activation=tf.nn.relu, seed=self.random_seed
+                    activation=tf.nn.tanh, seed=self.random_seed
                 )(input_sequence_layer)
                 conv_layer_5 = tf.keras.layers.GlobalMaxPooling1D(name='MaxPooling5')(conv_layer_5)
                 conv_layers.append(conv_layer_5)
@@ -852,7 +852,7 @@ class ImpatialTextClassifier(BaseEstimator, ClassifierMixin):
                 concat_layer = conv_layers[0]
             if self.hidden_layer_size > 0:
                 hidden_layer = tfp.layers.DenseFlipout(self.hidden_layer_size, seed=self.random_seed,
-                                                       name='HiddenLayer', activation=tf.nn.relu)(concat_layer)
+                                                       name='HiddenLayer', activation=tf.nn.tanh)(concat_layer)
                 output_layer = tfp.layers.DenseFlipout(len(self.classes_), seed=self.random_seed, name='OutputLayer')(
                     hidden_layer)
             else:
@@ -878,36 +878,36 @@ class ImpatialTextClassifier(BaseEstimator, ClassifierMixin):
             return train_op, elbo_loss, neg_log_likelihood, pi
         if self.filters_for_conv1 > 0:
             conv_layer_1 = tf.keras.layers.Conv1D(
-                filters=self.filters_for_conv1, kernel_size=1, name='Conv1', padding='valid', activation=tf.nn.relu,
-                kernel_initializer=tf.keras.initializers.he_uniform(seed=self.random_seed)
+                filters=self.filters_for_conv1, kernel_size=1, name='Conv1', padding='valid', activation=tf.nn.tanh,
+                kernel_initializer=tf.keras.initializers.glorot_normal(seed=self.random_seed)
             )(sequence_output)
             conv_layer_1 = tf.keras.layers.GlobalMaxPooling1D(name='MaxPooling1')(conv_layer_1)
             conv_layers.append(conv_layer_1)
         if self.filters_for_conv2 > 0:
             conv_layer_2 = tf.keras.layers.Conv1D(
-                filters=self.filters_for_conv2, kernel_size=2, name='Conv2', padding='valid', activation=tf.nn.relu,
-                kernel_initializer=tf.keras.initializers.he_uniform(seed=self.random_seed)
+                filters=self.filters_for_conv2, kernel_size=2, name='Conv2', padding='valid', activation=tf.nn.tanh,
+                kernel_initializer=tf.keras.initializers.glorot_normal(seed=self.random_seed)
             )(sequence_output)
             conv_layer_2 = tf.keras.layers.GlobalMaxPooling1D(name='MaxPooling2')(conv_layer_2)
             conv_layers.append(conv_layer_2)
         if self.filters_for_conv3 > 0:
             conv_layer_3 = tf.keras.layers.Conv1D(
-                filters=self.filters_for_conv3, kernel_size=3, name='Conv3', padding='valid', activation=tf.nn.relu,
-                kernel_initializer=tf.keras.initializers.he_uniform(seed=self.random_seed)
+                filters=self.filters_for_conv3, kernel_size=3, name='Conv3', padding='valid', activation=tf.nn.tanh,
+                kernel_initializer=tf.keras.initializers.glorot_normal(seed=self.random_seed)
             )(sequence_output)
             conv_layer_3 = tf.keras.layers.GlobalMaxPooling1D(name='MaxPooling3')(conv_layer_3)
             conv_layers.append(conv_layer_3)
         if self.filters_for_conv4 > 0:
             conv_layer_4 = tf.keras.layers.Conv1D(
-                filters=self.filters_for_conv4, kernel_size=4, name='Conv4', padding='valid', activation=tf.nn.relu,
-                kernel_initializer=tf.keras.initializers.he_uniform(seed=self.random_seed)
+                filters=self.filters_for_conv4, kernel_size=4, name='Conv4', padding='valid', activation=tf.nn.tanh,
+                kernel_initializer=tf.keras.initializers.glorot_normal(seed=self.random_seed)
             )(sequence_output)
             conv_layer_4 = tf.keras.layers.GlobalMaxPooling1D(name='MaxPooling4')(conv_layer_4)
             conv_layers.append(conv_layer_4)
         if self.filters_for_conv5 > 0:
             conv_layer_5 = tf.keras.layers.Conv1D(
-                filters=self.filters_for_conv5, kernel_size=5, name='Conv5', padding='valid', activation=tf.nn.relu,
-                kernel_initializer=tf.keras.initializers.he_uniform(seed=self.random_seed)
+                filters=self.filters_for_conv5, kernel_size=5, name='Conv5', padding='valid', activation=tf.nn.tanh,
+                kernel_initializer=tf.keras.initializers.glorot_normal(seed=self.random_seed)
             )(sequence_output)
             conv_layer_5 = tf.keras.layers.GlobalMaxPooling1D(name='MaxPooling5')(conv_layer_5)
             conv_layers.append(conv_layer_5)
@@ -915,11 +915,10 @@ class ImpatialTextClassifier(BaseEstimator, ClassifierMixin):
             concat_layer = tf.keras.layers.Concatenate(name='Concat')(conv_layers)
         else:
             concat_layer = conv_layers[0]
-        glorot_init = tf.keras.initializers.glorot_uniform(seed=self.random_seed)
+        glorot_init = tf.keras.initializers.glorot_normal(seed=self.random_seed)
         if self.hidden_layer_size > 0:
-            he_init = tf.keras.initializers.he_uniform(seed=self.random_seed)
-            hidden = tf.layers.dense(concat_layer, self.hidden_layer_size, kernel_initializer=he_init,
-                                     activation=tf.nn.relu, name='HiddenLayer')
+            hidden = tf.layers.dense(concat_layer, self.hidden_layer_size, kernel_initializer=glorot_init,
+                                     activation=tf.nn.tanh, name='HiddenLayer')
             logits = tf.layers.dense(hidden, len(self.classes_), kernel_initializer=glorot_init, name='Logits',
                                      activation=(tf.nn.sigmoid if self.multioutput else tf.nn.softmax))
         else:
